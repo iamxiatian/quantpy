4b381e3331903b28db4d01e9b428a276b1d951a5
jinbing <jinbing@sheyuan.com>
2016-06-09 00:37:17 +0800

    update module

--- quantpy/data.py
+++ quantpy/data.py
@@ -2,41 +2,14 @@
 # -*- coding: utf-8 -*-
 
 import pandas as pd
-# from ..orm import mongodb_connect
-# from ..domain import story_history_by_day
-import sys,pprint
 from mongoengine import *
-
-MONGODB_DB_NAME='stock'
-MONGODB_USER_NAME='stock_admin'
-MONGODB_PASSWORD='SstIq4r80iHMK7Xd'
-MONGODB_HOST='202.112.114.3'
-MONGODB_PORT=27017
-
-
-def get_mongodb_connection():
-    connect(MONGODB_DB_NAME,host=MONGODB_HOST, port=MONGODB_PORT, username=MONGODB_USER_NAME, password=MONGODB_PASSWORD)
-
-
-class StockHistoryByDay(Document):
-    # 交易日期
-    date = DateTimeField(max_length=200, required=True)
-    # 开盘价
-    open = DecimalField(max_length=200, required=False)
-    # 最高价
-    high = DecimalField(max_length=200, required=False)
-    # 最低价
-    low = DecimalField(max_length=200, required=False)
-    # 收盘价
-    close = DecimalField(max_length=200, required=False)
-    # 成交量
-    volume = LongField(max_length=200, required=False)
-    # 成交金额
-    amount = LongField(max_length=200, required=False)
-    name = StringField(max_length=200, required=False)
-    code = StringField(max_length=200, required=True)
-
-
+import sys
+import os
+PACKAGE_PARENT = '..'
+SCRIPT_DIR = os.path.dirname(os.path.realpath(os.path.join(os.getcwd(), os.path.expanduser(__file__))))
+sys.path.append(os.path.normpath(os.path.join(SCRIPT_DIR, PACKAGE_PARENT)))
+from domain import story_history_by_day
+from orm import mongodb_connect
 
 '''
 股票数据读写模块
@@ -48,8 +21,8 @@ def read(code,days = 180):
     '''
 	读取指定股票代码下的基本数据，返回dataframe，没一行数据包括：日期(date)、开盘价(open)、收盘价(close)、最高价(high)、最低价(low)、量(volume)
 	'''
-    get_mongodb_connection()
-    sh=StockHistoryByDay.objects(code=code)
+    mongodb_connect.get_mongodb_connection()
+    sh=story_history_by_day.StockHistoryByDay.objects(code=code)
     shList=[]
     for i in sh:
         shList.append(i.to_mongo())
@@ -58,5 +31,4 @@ def read(code,days = 180):
 
 
 if __name__ == '__main__':
-
     print(read('300425'))
